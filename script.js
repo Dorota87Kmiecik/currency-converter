f0d5c5cc1b54fece1f4ef719f03b7ce591ae5a7a
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2021-05-11 01:18:03 +0200

    adding new function

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 {
-    const init = () => {
 
-        const formElement = document.querySelector(".js-form");
-        formElement.addEventListener("submit", calculate);
-
-    }
-
-    const calculate = (event) => {
-        event.preventDefault();
+    const calculate = () => {
         const zlotyElement = document.querySelector(".js-money");
         const zloty = zlotyElement.value;
         const exchangeElement = document.querySelector(".js-exchange");
         const exchange = exchangeElement.value;
         const euro = zloty / exchange;
         addText(euro)
-    };
+    }
+
     const addText = (euro) => {
+
         const euroElement = document.querySelector(".js-strong");
         euroElement.innerText = euro.toFixed(2);
         const addParagraph = document.querySelector(".js-addParagraph")
         addParagraph.innerText = euro > 100 ? "Kup sobie cos fajnego" : "Pozyczyc Ci sianko?"
     }
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculate()
+    };
+
+    const init = () => {
+
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+
+    }
 
     init();
 };
